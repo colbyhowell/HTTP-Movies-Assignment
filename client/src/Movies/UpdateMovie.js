33c3f4aa0988ebfa5acc7e4a92bb408eb45a1635
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-10-11 18:58:10 -0500

    got major functionality done. MVP met

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,44 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useForm from "react-hook-form";
 import axios from "axios";
 
-function UpdateMovie(props) {
-  const id = props.match.params.id;
+function UpdateMovie({ match, history }) {
+  const id = match.params.id;
 
   const { register, handleSubmit, errors } = useForm();
+
+  const [movies, setMovies] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(res => setMovies(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
   const onSubmit = values => {
-    const starsArr = [values.stars1, values.stars2, values.stars3];
+    values.stars = values.stars.split(",");
     values.id = id;
-    values.stars = starsArr;
     axios
       .put(`http://localhost:5000/api/movies/${id}`, values)
       .then(res => {
         console.log(res);
-        props.history.push("/");
+        history.push("/");
       })
       .catch(err => console.log(err, values));
   };
-  console.log(props.match.params);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="title" name="title" ref={register} />
+      <input
+        type="text"
+        placeholder="title"
+        name="title"
+        ref={register}
+        defaultValue={movies.title}
+      />
       <input
         type="text"
         placeholder="director"
         name="director"
-        value={props.match.params.title}
+        defaultValue={movies.director}
         ref={register}
       />
       <input
         type="number"
         placeholder="metascore"
         name="metascore"
+        defaultValue={movies.metascore}
         ref={register}
       />
-      <input type="text" placeholder="stars" name="stars1" ref={register} />
-      <input type="text" placeholder="stars" name="stars2" ref={register} />
-      <input type="text" placeholder="stars" name="stars3" ref={register} />
+      <input
+        type="text"
+        placeholder="stars"
+        name="stars"
+        ref={register}
+        defaultValue={movies.stars}
+      />
       <input type="submit" />
     </form>
   );
